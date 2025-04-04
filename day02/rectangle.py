0cc58c32e4343dd5dde4aa1e83a41df3309737fe
andreisrnk <andreysarenko11@gmail.com>
2025-04-04 13:52:41 +0300

    Create rectangle.py

---
+++ day02/rectangle.py
@@ -0,0 +1,8 @@
+height = float(input("Enter the height of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = height * width
+perimeter = 2 * (height + width)
+
+print(f"Area of the rectangle: {area:.2f}")
+print(f"Perimeter of the rectangle: {perimeter:.2f}")
