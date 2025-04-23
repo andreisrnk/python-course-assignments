674a8784008102bfadc6f2a4486883eb33aae74e
andreisrnk <andreysarenko11@gmail.com>
2025-04-23 09:51:44 +0300

    number_guessing_game_0

--- day03/number_guessing_game_0.py
+++ day03/number_guessing_game_0.py
@@ -1,15 +1,14 @@
 import random
 
-# Computer chooses a random number between 1 and 20
-secret_number = random.randint(1, 20)
+secret_number = random.randint(1, 20) # The program chooses a random number between 1 and 20
+a = int(input("Guess a number between 1 and 20: ")) # Asking the user to guess a number
 
-# Prompt the user for a guess
-guess = int(input("Guess a number between 1 and 20: "))
-
-# Provide feedback
-if guess < secret_number:
-    print("Too low!")
-elif guess > secret_number:
-    print("Too high!")
+if a<1 or a>20:
+    print("Out of range!")
 else:
-    print("Correct! You guessed the number.")
+    if a<secret_number:
+        print("Too low!")
+    elif a>secret_number:
+        print("Too high!")
+    else:
+        print("Correct! You guessed the number.")
