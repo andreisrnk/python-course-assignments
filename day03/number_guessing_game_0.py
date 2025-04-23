2ecc102b0728e1103f0973971e8aa3f68ebf6e62
andreisrnk <andreysarenko11@gmail.com>
2025-04-23 09:41:47 +0300

    Add number guessing game level 0

--- day03/number_guessing_game_0.py
+++ day03/number_guessing_game_0.py
@@ -1,1 +1,15 @@
+import random
 
+# Computer chooses a random number between 1 and 20
+secret_number = random.randint(1, 20)
+
+# Prompt the user for a guess
+guess = int(input("Guess a number between 1 and 20: "))
+
+# Provide feedback
+if guess < secret_number:
+    print("Too low!")
+elif guess > secret_number:
+    print("Too high!")
+else:
+    print("Correct! You guessed the number.")
