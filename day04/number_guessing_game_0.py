ff0c9ea9dac3d57d3fde2215ec76bbf6aa7a0d23
andreisrnk <andreysarenko11@gmail.com>
2025-05-14 17:55:19 +0300

    Update and rename .gitkeep to number_guessing_game_0.py

---
+++ day04/number_guessing_game_0.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(1, 20)
+while True:
+    guess = input("Guess a number between 1 and 20: ").strip()
+    if not guess.isdigit():
+        print("Please enter a valid whole number.")
+        continue
+
+    guess = int(guess)
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Correct!")
+    break
