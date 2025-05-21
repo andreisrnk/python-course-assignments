f217abaf8b8adbe395d73b514c2a78104e8f753b
andreisrnk <andreysarenko11@gmail.com>
2025-05-21 17:19:11 +0300

    Update dna_sequencing.py

--- day05/dna_sequencing.py
+++ day05/dna_sequencing.py
@@ -1,1 +1,16 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print(f"ERROR: {sys.argv[0]}")
+    sys.exit(1)
+
+sequence = sys.argv[1]
+# return the sequences containing only ACTG
+runs = re.findall(r"[ACTG]+", sequence)
+print(f"the sequences containing only ACTG:  {runs}")
+# sort runs by length (descending) and output
+sorted_runs = sorted(runs, key=len, reverse=True)
+print(sorted_runs)
+
 
